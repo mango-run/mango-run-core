1022d8a537e7f78cbc7b47c71d414e430b3342df
yujuiting <yujuiting@me.com>
2022-02-27 21:29:51 +0800

    clear app position

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,12 @@
-import { Signal, Market, SignalEventListener } from 'types'
+import { Signal, Market, SignalEventListener, OrderType, OrderSide, Logger } from 'types'
 
 export class Bot {
   placeOrderHandler: SignalEventListener<'place_order_event'> = ({ order }) => this.market.placeOrder(order)
   cancelOrderHandler: SignalEventListener<'cancel_order_event'> = ({ id }) => this.market.cancelOrder(id)
   cancelAllOrdersHandler: SignalEventListener<'cancel_all_orders_event'> = () => this.market.cancelAllOrders()
+  clearAllPositionHandler: SignalEventListener<'clear_all_position'> = () => this.clearAllPosition()
 
-  constructor(protected market: Market, protected signal: Signal) {}
+  constructor(protected market: Market, protected signal: Signal, protected logger: Logger) {}
 
   async start() {
     this.signal.on('place_order_event', this.placeOrderHandler)
@@ -19,5 +20,37 @@ export class Bot {
     this.signal.off('place_order_event', this.placeOrderHandler)
     this.signal.off('cancel_order_event', this.cancelOrderHandler)
     this.signal.off('cancel_all_orders_event', this.cancelAllOrdersHandler)
+  }
+
+  async clearAllPosition() {
+    this.logger.info('clearing all position')
+
+    let isFulfilled = false
+
+    while (!isFulfilled) {
+      const [{ base }, bestBid] = await Promise.all([this.market.balance(), this.market.bestBid()])
+
+      if (base === 0) {
+        isFulfilled = true
+        break
+      }
+
+      if (bestBid) {
+        await this.market.placeOrder({
+          type: OrderType.IOC,
+          side: OrderSide.Sell,
+          price: bestBid.price,
+          size: Math.min(bestBid.size, base),
+        })
+      } else {
+        this.logger.debug('not found best bid')
+      }
+    }
+
+    const balance = await this.market.balance()
+
+    this.logger.info(`all position cleared, balance.base: ${balance.base}, balance.quote: ${balance.quote}`)
+
+    return isFulfilled
   }
 }
