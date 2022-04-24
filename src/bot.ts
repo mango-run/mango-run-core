07d931af47111f1c5428a2c5a6ef918f93f37553
Aaron <aaron821124@gmial.com>
2022-04-24 14:13:46 +0800

    remove bot event emitter, use getBotStatus

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,3 @@
-import { EventEmitter } from 'events'
 import { Signal, Market, SignalEventListener, Logger, Callback } from './types'
 import { doDestroy, doInitialize, measureTime } from './utils'
 
@@ -7,18 +6,19 @@ export interface CallbackWithDetail {
   callback: Callback
 }
 
-export type BotEventMap = {
-  status: { message: string }
+export type BotStatusMap = {
+  init: void
+  starting: void
+  started: void
+  running: void
+  stopping: void
+  stopped: void
 }
 
-export type BotEvent = keyof BotEventMap
-
-export type BotEventPayload<E extends BotEvent> = BotEventMap[E]
-
-export type BotEventListener<E extends BotEvent> = (payload: BotEventPayload<E>) => void
+export type BotStatus = { status: keyof BotStatusMap; message: string }
 
 export class Bot {
-  eventEmitter = new EventEmitter()
+  status: BotStatus
 
   blockCallbacks: CallbackWithDetail[] = []
 
@@ -42,10 +42,11 @@ export class Bot {
 
   constructor(protected market: Market, protected signal: Signal, logger: Logger) {
     this.logger = logger.create('bot')
+    this.status = { status: 'init', message: '' }
   }
 
   blockSignal(callback: Callback, name = 'unknown') {
-    this.eventEmitter.emit('status', { message: 'running | blockSignal' })
+    this.status = { status: 'running', message: 'blockSignal' }
     this.blockCallbacks.push({ callback, name })
 
     if (this.signal.isPaused) return
@@ -62,7 +63,7 @@ export class Bot {
   }
 
   async resolveBlockSignalCallbacks() {
-    this.eventEmitter.emit('status', { message: 'running | resolveBlockSignalCallbacks' })
+    this.status = { status: 'running', message: 'resolveBlockSignalCallbacks' }
     this.logger.debug('start to resolve block signal callbacks')
 
     const callbacks = [...this.blockCallbacks]
@@ -89,7 +90,7 @@ export class Bot {
   }
 
   async start() {
-    this.eventEmitter.emit('status', { message: 'starting | wait for market and signal to init' })
+    this.status = { status: 'starting', message: 'wait for market and signal to init' }
     await Promise.all([
       measureTime(() => doInitialize(this.market)).then(dt => this.logger.info('market initialized', `take ${dt}s`)),
       measureTime(() => doInitialize(this.signal)).then(dt => this.logger.info('signal initialized', `take ${dt}s`)),
@@ -101,13 +102,13 @@ export class Bot {
     this.signal.on('cancel_all_orders_event', this.cancelAllOrdersHandler)
     this.signal.on('clear_all_position', this.clearAllPositionHandler)
 
-    this.eventEmitter.emit('status', { message: 'starting | wait for signal to start' })
+    this.status = { status: 'starting', message: 'wait for market and signal to init' }
     await this.signal.start()
-    this.eventEmitter.emit('status', { message: 'started' })
+    this.status = { status: 'started', message: '' }
   }
 
   async stop() {
-    this.eventEmitter.emit('status', { message: 'stopping | wait for signal to stop' })
+    this.status = { status: 'stopping', message: 'wait for wait for signal to stop' }
     await this.signal.stop()
     this.signal.off('place_order_event', this.placeOrderHandler)
     this.signal.off('cancel_order_event', this.cancelOrderHandler)
@@ -115,27 +116,15 @@ export class Bot {
     this.signal.off('clear_all_position', this.clearAllPositionHandler)
     // @todo it should be optional
     // await measureTime(() => this.clearAllPosition()).then(dt => this.logger.info('clear all position', `take ${dt}s`))
-    this.eventEmitter.emit('status', { message: 'stopping | wait for market and signal to destroy' })
+    this.status = { status: 'stopping', message: 'wait for market and signal to destroy' }
     await Promise.all([
       measureTime(() => doDestroy(this.market)).then(dt => this.logger.info('market destroyed', `take ${dt}s`)),
       measureTime(() => doDestroy(this.signal)).then(dt => this.logger.info('signal destroyed', `take ${dt}s`)),
     ])
-    this.eventEmitter.emit('status', { message: 'stopped' })
+    this.status = { status: 'stopped', message: '' }
   }
 
-  on<E extends BotEvent>(event: E, listener: BotEventListener<E>): void {
-    this.eventEmitter.on(event, listener)
-  }
-
-  off<E extends BotEvent>(event: E, listener: BotEventListener<E>): void {
-    this.eventEmitter.off(event, listener)
-  }
-
-  once<E extends BotEvent>(event: E, listener: BotEventListener<E>): void {
-    this.eventEmitter.once(event, listener)
-  }
-
-  emit<E extends BotEvent>(event: E, payload: BotEventListener<E>) {
-    this.eventEmitter.emit(event, payload)
+  getBotStatus() {
+    return this.status
   }
 }
